bb18dc12525cab76c99bf716e8aff32075c70143
Artemii <velikanov19@gmail.com>
2019-12-08 14:28:45 +0900

    deleted cache files

--- .history/parse_logs_20191208123458.py
+++
@@ -1,8 +0,0 @@
-from ast import literal_eval
-
-def parse_log_data():
-    list_of_population_fitnesses = []
-    for line in open("fitness.log", "r"):
-        population_fitnesses = literal_eval(line)
-        list_of_population_fitnesses.append(population_fitnesses)
-    return list_of_population_fitnesses
